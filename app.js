6c4797027fb1c8790a86dc38e9782d828389bcc9
Arun Vel <arunvelv9253@gmail.com>
2023-08-26 22:15:58 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
 const express = require("express");
-const bodyparser = require("body-parser");
-const { Console } = require("console");
-const date = require(__dirname + "/date.js");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
-
 app.set("view engine", "ejs");
 
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", {
-    listTitle: day,
-    newListItems: items,
-  });
-});
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const defaultItems = [
+  { name: "Welcome to your todolist!" },
+  { name: "Hit the + button to add a new item." },
+  { name: "<-- Hit this to delete an item." }
+];
+
+app.get("/", async function (req, res) {
+  try {
+    const foundItems = await Item.find({});
+    if (foundItems.length === 0) {
+      await Item.insertMany(defaultItems);
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", 
-  newListItems: workItems });
+app.post("/", async function (req, res) {
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+  try {
+    await item.save();
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(3000, function () {
-  console.log("Server is runnning in port 3000");
+  console.log("Server is running on port 3000");
 });
